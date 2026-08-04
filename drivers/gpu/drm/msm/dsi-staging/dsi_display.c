2ecfa56139ec69dc8977a268a9cd8846348ca08c
Kujang Team <emuandro102030@gmail.com>
2026-08-04 14:28:16 +0700

    Update dsi_display.c roleback to default

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -6987,9 +6987,7 @@ int dsi_display_set_mode(struct dsi_display *display,
 	}
 
 #ifdef CONFIG_MACH_XIAOMI_SWEET
-	if (adj_mode.timing.refresh_rate == 60 ||
-    adj_mode.timing.refresh_rate == 90 ||
-    adj_mode.timing.refresh_rate == 120)
+	if (adj_mode.timing.refresh_rate == 60)
 		dsi_panel_gamma_mode_change(display->panel, &adj_mode);
 #endif
 
